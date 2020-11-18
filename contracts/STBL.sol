159294f38a26b4408db88f38db77c8de6181eb7c
Öhlsläger Tomáš <h881446@stud.u-szeged.hu>
2020-11-18 20:28:24 +0100

    Deposit/withdraw stbl tokens

--- contracts/STBL.sol
+++ contracts/STBL.sol
@@ -1,8 +1,40 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.7;
+pragma solidity >=0.6.7;
 
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./EthPriceOracle.sol";
 
 contract STBL is ERC20 {
 
+    EthPriceOracle internal ethPriceOracle;
+
+    /// @dev We should decide if we need initial supply
+    constructor(string memory name, string memory symbol) public
+    ERC20(name, symbol)
+    {
+        _mint(address(this), 100000 * 10 ** uint(decimals()));
+        ethPriceOracle = new EthPriceOracle();
+    }
+
+    /// @notice Gives STBL to the account that calls it, accepts ETH
+    /// @dev Should use transferFrom instead of _mint if we want to work with fixed supply
+    function deposit(uint _amount) public payable {
+        require(msg.value == _amount);
+        uint stblAmount = _amount * ethPriceOracle.getLatestPrice();
+        require(balanceOf(address(this)) >= stblAmount, "Not enough tokens available");
+        //transferFrom(address(this), msg.sender, stblAmount);
+        _mint(msg.sender, stblAmount);
+    }
+
+    /// @notice Removes STBL from the account that calls it, sends ETH
+    /// @dev Should use transferFrom instead of _burn if we want to work with fixed supply
+    function withdraw(uint _amount) public {
+        uint ethAmount = _amount / ethPriceOracle.getLatestPrice();
+        require(address(this).balance >= ethAmount, "Not enough ether available");
+        msg.sender.transfer(ethAmount);
+        //transferFrom(msg.sender, address(this), _amount);
+        _burn(msg.sender, _amount);
+    }
+    
 }
