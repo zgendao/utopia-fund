37bc395c9e048705fd3e627c59b5c2b2a76e3165
Öhlsläger Tomáš <h881446@stud.u-szeged.hu>
2020-12-14 17:25:53 +0100

    Removed Controller

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,7 @@
 const vault = artifacts.require("Vault");
-const controller = artifacts.require("Controller");
 const strategy = artifacts.require("Strategy");
 
 module.exports = function(deployer) {
   deployer.deploy(vault);
-  deployer.deploy(controller);
   deployer.deploy(strategy);
 };
